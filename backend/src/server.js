1fdf920c00d6f2d977c283116c85adf99e22c112
Ved-sketch <vedgupta1806@gmail.com>
2025-12-15 01:35:01 +0530

    made the ui for the home-page. Made the cards, also added cors.

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,17 +3,19 @@ import notesRoutes from "./routes/notesRoutes.js";
 import {connectDB} from "./config/db.js"
 import dotenv from "dotenv"
 import rateLimiter from './middleware/rateLimiter.js';
+import cors from "cors"
 
 dotenv.config();
 
 const port = process.env.PORT || 5001; 
 const app = express();
 
-connectDB();
-
 //middleware
+app.use(cors({
+  origin: "http://localhost:5173",
+}));
 app.use(express.json())
-app.use(rateLimiter)
+app.use(rateLimiter) 
 
 // custom middleware
 // app.use((req,res,next) =>{
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,14 +1,55 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from '../Components/Navbar'
 import RateLimitedUI from '../Components/RateLimitedUI';
+import axios from "axios"
+import NoteCard from '../Components/NoteCard';
 
 const HomePage = () => {
     const [isRateLimited,setRateLimited] = useState(false);
+    const [notes,setNotes] = useState([])
+    const [loading,setLoading] = useState(true)
+
+    useEffect(() => {
+        const fetchNotes = async () =>{
+            try{
+                const res = await axios.get("http://localhost:5001/api/notes")
+                console.log(res.data);
+                setNotes(res.data);
+                setRateLimited(false);  
+            }
+            catch(error){
+                console.log("Error fetching notes")
+                if(error.response.status === 429){
+                    setRateLimited(true)
+                }
+                else{
+                    toast.error("Failed to load notes")
+                }
+            }
+            finally{
+                setLoading(false);
+            }
+        }
+
+        fetchNotes();
+    },[])
   return (
     <div className='min-h-screen'>
         <Navbar/>
 
         {isRateLimited && <RateLimitedUI/>}
+
+        <div className='max-w-7xl mx-auto p-4 mt-6'>
+            {loading && <div className='text-center text-primary py-10'>Loading notes...</div>}
+
+            {notes.length > 0 && !isRateLimited && (
+                <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
+                    {notes.map(note => (
+                        <NoteCard key={note._id} note={note}/>
+                    ))}
+                </div>
+            )}
+        </div>
     </div>
   )
 }
