ba141ec43861cd1eaa4e96d00492a092e33fcc6c
Ved-sketch <vedgupta1806@gmail.com>
2025-12-18 03:27:55 +0530

    Fix Express 5.x wildcard route and __dirname typo

--- backend/src/server.js
+++ backend/src/server.js
@@ -10,7 +10,7 @@ dotenv.config();
 
 const port = process.env.PORT || 5001; 
 const app = express();
-const _dirname = path.resolve();
+const __dirname = path.resolve();
 
 //middleware
 if (process.env.NODE_ENV !== 'production') {
@@ -32,9 +32,9 @@ app.use("/api/notes", notesRoutes);
 
 
 if (process.env.NODE_ENV === 'production') { 
-    app.use(express.static(path.join(_dirname, '../frontend/dist')));
-    app.get('*', (req, res) => {
-    res.sendFile(path.join(_dirname, '../frontend', 'dist', 'index.html'));
+    app.use(express.static(path.join(__dirname, '../frontend/dist')));
+    app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend', 'dist', 'index.html'));
   });
 }
 
